a3b3c7dd837d35ba54562489f452dee1cd676060
nickaustin37 <45698254+nickaustin37@users.noreply.github.com>
2022-11-15 23:59:56 -0600

    Update streamlit_app.py
    removed "apple"

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
 # - removed - streamlit.write('The user entered ', fruit_choice)
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
