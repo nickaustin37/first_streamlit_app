a13b6138283c9b0b0250307fe46f4c7eab101952
nickaustin37 <45698254+nickaustin37@users.noreply.github.com>
2022-11-15 23:58:22 -0600

    Update streamlit_app.py
    added error and try if not else logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,17 +29,21 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json()). # no longer need
-
-# normalize the json response 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# this is a dataframe, output as a table
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
+# - removed - streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice
+    streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # streamlit.text(fruityvice_response.json()). # no longer need
+      # normalize the json response 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # this is a dataframe, output as a table
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlist.error()
+  
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
