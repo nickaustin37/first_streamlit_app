6354fc8419a309589c4346e33701fafb2436fd84
nickaustin37 <45698254+nickaustin37@users.noreply.github.com>
2022-11-15 22:20:40 -0600

    Update streamlit_app.py
    commented response line and changed response to kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
+# streamlit.text(fruityvice_response.json()). # no longer need
 
-# write your own comment -what does the next line do? 
+# normalize the json response 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# this is a dataframe, output as a table
 streamlit.dataframe(fruityvice_normalized)
