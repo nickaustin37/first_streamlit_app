4f4cb4fb8f7e03ea492d598d0fd7639c76210732
nickaustin37 <45698254+nickaustin37@users.noreply.github.com>
2022-11-16 00:05:36 -0600

    Update streamlit_app.py
    adding a function and removing some lines of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,20 +27,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-#new section to display fruityvice api response
+
+# create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized  
+    
+# new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-# - removed - streamlit.write('The user entered ', fruit_choice)
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      # streamlit.text(fruityvice_response.json()). # no longer need
-      # normalize the json response 
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      # this is a dataframe, output as a table
-      streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlist.error()
   
